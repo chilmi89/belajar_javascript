b8bdfeeb83970ecaecf618a9e40770d463bb8264
chilmi89 <achmadchilmi87@gmail.com>
2024-11-02 23:47:39 +0700

    belajar modularisas import

---
+++ modul/main.mjs
@@ -0,0 +1,16 @@
+import result from './main2.mjs';
+import {myFunction2} from './main2.mjs';
+import { nama , umur , alamat } from './main2.mjs';
+import * as variables from './main2.mjs';
+
+
+
+console.log(variables.nama);
+console.log(variables.umur);
+console.log(variables.alamat);
+
+console.log(nama , umur , alamat);
+myFunction2();
+result();
+
+
---
+++ modul/main2.mjs
@@ -0,0 +1,16 @@
+export default function myFunction() {
+    console.log('Ini adalah function export default.');
+  }
+
+
+export function myFunction2() {
+    console.log('Ini adalah function export default dari contoh modul import.');
+  }
+
+
+  const nama = "achmad";
+  const umur = 20;
+  const alamat = "kediri";
+
+
+  export {nama , umur , alamat};
