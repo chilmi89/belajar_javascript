227190070e4e49c9d351f03ecdf726c8d9f58ed1
chilmi89 <achmadchilmi87@gmail.com>
2024-11-02 11:18:34 +0700

    belajar flow control if statement

---
+++ flowcontrol/if.js
@@ -0,0 +1,30 @@
+const gajian = 10000;
+
+
+// belajar if statement
+
+if (gajian == 10000) {
+    console.log('gajian 10000');
+} else {
+    console.log('tidak ada gajian');
+}
+
+// belajar if else
+const nilai_ujian = 80;
+
+if (nilai_ujian >= 80){
+    console.log('selamat anda lulus ujian')
+}else{
+    console.log('anda harus mengulang ujian')
+}
+
+
+
+// belajar if else if   
+const gaji_pegawai = 2300000;
+
+if (gaji_pegawai >= 2300000){
+    console.log('pegewai termasuk ke dalam karyawan tetap')
+} else if (gaji_pegawai < 2300000){
+    console.log('pegewai termasuk ke dalam karyawan kontrak')
+}
