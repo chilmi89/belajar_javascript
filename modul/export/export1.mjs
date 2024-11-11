e49aa606e263b0e03f2037d3b538b6d72210f257
chilmi89 <achmadchilmi87@gmail.com>
2024-11-11 17:48:07 +0700

    belajar export dan import

---
+++ modul/export/export1.mjs
@@ -0,0 +1,5 @@
+export const name = 'achmad chilmi';
+
+export const Daftar_Makanan = ['roti' , 'sate' , 'soto'];
+
+
---
+++ modul/export/export2.mjs
@@ -0,0 +1,9 @@
+const name = 'achmad chilmi';
+const Daftar_Makanan = ['roti' , 'sate' , 'soto'];
+
+function sayHELLO(name){
+    console.log('hello world' , name);  
+}
+
+
+export {name , Daftar_Makanan , sayHELLO};
